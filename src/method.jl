07bf5f8b59673c2e98dd981ac5e5776df646bb85
lalvim <alvim.lgm@gmail.com>
2017-11-07 14:53:26 -0200

    removing test

--- src/method.jl
+++ src/method.jl
@@ -150,12 +150,3 @@ function transform{T<:AbstractFloat}(pls::Type{PLS}, X::DenseMatrix{T})
     return pls1_predictor(pls,Xi,Yi)::Vector{AbstractFloat}
 
 end
-
-
-X        = [1 2; 2 4.5; 4.7 9.3]
-Y        = [2.1; 4.6; 9.4]
-
-#fit(X, Y, nfactors=1,copydata=true)
-fit(X, Y, nfactors=1)
-
-#, 10, copydata=true)
