4437d8bbe77a0d769d4293bf4668575d6f86bb86
lalvim <alvim.lgm@gmail.com>
2017-11-07 14:53:11 -0200

    adding test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,11 @@ using PLS
 using Base.Test
 
 # write your own tests here
-@test 1 == 2
+
+X        = [1 2; 2 4.5; 4.7 9.3]
+Y        = [2.1; 4.6; 9.4]
+
+#fit(X, Y, nfactors=1,copydata=true)
+fit(X, Y, nfactors=1)
+
+#, 10, copydata=true)
