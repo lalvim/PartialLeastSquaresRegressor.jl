63823d28558c8766a383ad1da5f306f7584921c6
Leandro Alvim <alvim.lgm@gmail.com>
2017-11-14 01:43:38 -0200

    Doing tests. The current PLS2 tests are ok.

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using JLD
 using Base.Test
 reload("PLS")
 
-@testset "PLS2 Pediction Tests (in sample)" begin
+@testset "PLS2 Prediction Tests (in sample)" begin
 
 	@testset "Single Column Prediction Test" begin
 
@@ -11,11 +11,86 @@ reload("PLS")
 		Y        = [1 1; 2 2; 3 3.0]
 		model    = PLS.fit(X,Y,nfactors=1)
 		pred     = PLS.transform(model,X)
+
 		@test isequal(round.(pred),[1 1; 2 2; 3 3.0])
 
 	end
 
+	@testset "Constant Values Prediction Tests (Ax + b) | A=0, b=1 " begin
 
+		X        = [1 3;2 1;3 2.0]
+		Y        = [1 1; 1 1; 1 1.0]
+		try PLS.fit(X,Y,nfactors=2) catch @test true end
+
+	end
+
+	@testset "Linear Prediction Tests " begin
+
+
+		X        = [1 2; 2 4; 4 6.0]
+		Y        = [4 2;6 4;8 6.0]
+		model    = PLS.fit(X,Y,nfactors=2)
+		pred     = PLS.transform(model,X)
+		@test isequal(round.(pred),[4 2;6 4;8 6.0])
+
+		X           = [1 -2; 2 -4; 4 -6.0]
+		Y           = [-4 -2;-6 -4;-8 -6.0]
+		model       = PLS.fit(X,Y,nfactors=2)
+		pred        = PLS.transform(model,X)
+		@test isequal(round.(pred),[-4 -2;-6 -4;-8 -6.0])
+
+
+	end
+
+
+end
+
+@testset "PLS2 Pediction Tests (out of sample)" begin
+
+
+	@testset "Linear Prediction Tests (Ax + b) | A>0" begin
+
+
+		Xtr        = [1 2;2 4;3 6;6 12;7 14.0]
+		Ytr        = [2 2;4 4;6 6;12 12;14 14.0]
+		Xt         = [4 8;5 10.0]
+		model    = PLS.fit(Xtr,Ytr,nfactors=2)
+		pred     = PLS.transform(model,Xt)
+		@test isequal(round.(pred),[8 8;10 10.0])
+
+        #=
+		Xtr        = [1 2; 2 4; 4.0 6]
+		Ytr        = [6 4;8 6;10 8.0]
+		Xt         = [6 8; 8 10; 10.0 12]
+		model    = PLS.fit(Xtr,Ytr,nfactors=2)
+		pred     = PLS.transform(model,Xt)
+		@test isequal(round.(pred),[12 10;14 12;16 14.0])
+        =#
+
+	end
+    #=
+	@testset "Linear Prediction Tests (Ax + b) | A<0" begin
+
+
+
+		Xtr        = [1 -2; 2 -4; 4.0 -6]
+		Ytr        = [-4 -2;-6 -4;-8 -6.0]
+		Xt         = [6 -8; 8 -10; 10.0 -12]
+		model    = PLS.fit(Xtr,Ytr,nfactors=2)
+		pred     = PLS.transform(model,Xt)
+		@test isequal(round.(pred),[-10 -8;-12 -10;-14 -12.0])
+
+
+		Xtr        = [1 -2; 2 -4; 4.0 -6]
+		Ytr        = [-6 -4;-8 -6;-10 -8.0]
+		Xt         = [6 -8; 8 -10; 10.0 -12]
+		model    = PLS.fit(Xtr,Ytr,nfactors=2)
+		pred     = PLS.transform(model,Xt)
+		@test isequal(round.(pred),[-12 -10;-14 -12;-16 -14.0])
+
+	end
+    =#
+end;
 
 
 const MODEL_FILENAME = "pls_model.jld" # jld filename for storing the model
