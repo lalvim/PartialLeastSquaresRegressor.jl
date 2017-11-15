e5bed9dd46f69d57e286af3534a25d628c06d06f
Leandro Alvim <alvim.lgm@live.com>
2017-11-15 15:53:34 -0200

    Kernel matrix function fix

--- src/utils.jl
+++ src/utils.jl
@@ -3,10 +3,10 @@
 ## Auxiliary functions
 # A gaussian kernel function
 @inline function Φ(x::Vector{Float64},
-                    y::Vector{Float64},
-                    w::Float64=1.0)
-    norm  = 1.0 / sqrt(2.0 * pi * w)
-    scale = 1.0 / (2.0 * w^2)
+                   y::Vector{Float64},
+                   r::Float64=1.0)
+    norm  = 1.0 / sqrt(2π*r)
+    scale = 1.0 / (2r^2)
     return norm * exp(-scale * sum((x.-y).^2))
 end
 # A kernel matrix
@@ -15,10 +15,10 @@ function ΦΦ(X::Matrix{Float64})
     K = zeros(n,n)
     for i=1:n
         for j=1:i
-                K[i, j] = Φ(Xc[i, :], Xc[j, :])
-                K[j, i] = K[i, j]
-            K[i, i] = Φ(Xc[i, :], Xc[i, :])
+            K[i, j] = Φ(X[i, :], X[j, :])
+            K[j, i] = K[i, j]
         end
+        K[i, i] = Φ(X[i, :], X[i, :])
     end
     K
 end
