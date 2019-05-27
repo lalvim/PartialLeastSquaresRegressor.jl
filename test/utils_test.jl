15648cd3279a5de9377d65f4cdfe210a39e97309
leandro <alvim.lgm@gmail.com>
2019-05-27 14:22:59 -0300

    julia update code from 0.7 to 1.1

--- test/utils_test.jl
+++ test/utils_test.jl
@@ -5,44 +5,44 @@
     @testset "check constant columns" begin
 
 		try
-			PLSRegressor.check_constant_cols(Matrix([1.0 1;1 2;1 3]))
+			PLSRegressor.check_constant_cols([1.0 1;1 2;1 3])
 		catch
 			@test true
 		end
 		try
-			PLSRegressor.check_constant_cols(Matrix([1.0;1;1]))
+			PLSRegressor.check_constant_cols([1.0;1;1][:,:])
 		catch
 			@test true
 		end
 		try
-			PLSRegressor.check_constant_cols(Matrix([1.0 2 3]))
+			PLSRegressor.check_constant_cols([1.0 2 3])
 		catch
 			@test true
 		end
 		try
-			PLSRegressor.check_constant_cols([1.0; 1; 1])
+			PLSRegressor.check_constant_cols([1.0; 1; 1][:,:])
 		catch
 			@test true
 		end
 
 		@test PLSRegressor.check_constant_cols([1.0 1;2 2;3 3])
-		@test PLSRegressor.check_constant_cols([1.0;2;3])
+		@test PLSRegressor.check_constant_cols([1.0;2;3][:,:])
 
 	end
 
 	@testset "centralize" begin
 
-		X        = reshape([1; 2; 3.0],(3,1))
-		X        = PLSRegressor.centralize_data(X,mean(X,1),std(X,1))
-		@test all(X .== [-1,0,1.0])
+		X        = [1; 2; 3.0][:,:]
+		X        = PLSRegressor.centralize_data(X,mean(X,dims=1),std(X,dims=1))
+		@test all(X .== [-1;0;1.0])
 
 	end
 
 	@testset "decentralize" begin
 
 		Xo        = [1; 2; 3.0][:,:]
-		Xn        = [-1,0,1.0][:,:]
-		Xn        = PLSRegressor.decentralize_data(Xn,mean(Xo,1),std(Xo,1))
+		Xn        = [-1;0;1.0][:,:]
+		Xn        = PLSRegressor.decentralize_data(Xn,mean(Xo,dims=1),std(Xo,dims=1))
 		@test all(Xn .== [1; 2; 3.0])
 
 	end
@@ -72,16 +72,16 @@
 	@testset "checkparams" begin
 
 		 try
-			 PLSRegressor.check_data(Matrix{Float64}(0,0), 0)
+			 PLSRegressor.check_data(zeros(0,0), 0)
 		 catch
 			 @test true
 		 end
 		 try
-			 PLSRegressor.check_data(Matrix{Float64}(1,1), 10)
+			 PLSRegressor.check_data(zeros(1,1), 10)
 		 catch
 			 @test true
 		 end
-		 @test PLSRegressor.check_data(Matrix{Float64}(1,1), 1)
+		 @test PLSRegressor.check_data(zeros(1,1), 1)
 
 	end
 
