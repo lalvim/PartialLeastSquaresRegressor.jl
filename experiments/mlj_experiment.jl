4bf8418e6f7277f0d9fad781c70147da984c6158
alvim <alvim.lgm@gmail.com>
2020-05-30 22:44:00 -0300

    Update mlj_experiment.jl

--- experiments/mlj_experiment.jl
+++ experiments/mlj_experiment.jl
@@ -11,23 +11,23 @@ data = RDatasets.dataset("datasets", "longley");
 y, X = unpack(data, ==(:GNP), colname -> true);
 
 # algorothm
-pls_model      = PLS(n_factors=1,centralize=true,copy_data=true,rng=42)
+pls_model      = PLS(n_factors=10,centralize=true,copy_data=true,rng=42)
 
 # associating algo. and data
 pls_machine    = MLJ.machine(pls_model, X, y)
 
 # evaluate you regressor using cross validation
-MLJ.evaluate!(pls_machine, resampling=CV(shuffle=true), measure=mae, verbosity=0)
+MLJ.evaluate!(pls_machine, resampling=CV(shuffle=true), measure=[mae], verbosity=0)
 
 
 # you can use hould out
-train, test    = partition(eachindex(y), 0.7, shuffle=true); 
+train, test    = MLJ.partition(eachindex(y), 0.7, shuffle=true); 
 
-fit!(pls_machine, rows=train)
+MLJ.fit!(pls_machine, rows=train)
 
-yhat = predict(pls_machine, rows=test);
+yhat = MLJ.predict(pls_machine, rows=test);
 
-mae(yhat, y[test]) |> mean
+MLJ.mae(yhat, y[test]) |> mean
 
 
 
