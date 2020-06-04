b93a6b8010ebcc91aa5430de891cf51337483538
Filipe Braida <filipebraida@gmail.com>
2020-06-04 17:46:29 -0300

    Refactoring exception tests

--- test/utils_test.jl
+++ test/utils_test.jl
@@ -1,86 +1,41 @@
 @testset "Auxiliary Functions Test" begin
-
     @testset "check constant columns" begin
-
-		try
-			PLSRegressor.check_constant_cols([1.0 1;1 2;1 3])
-		catch
-			@test true
-		end
-		try
-			PLSRegressor.check_constant_cols([1.0;1;1][:,:])
-		catch
-			@test true
-		end
-		try
-			PLSRegressor.check_constant_cols([1.0 2 3])
-		catch
-			@test true
-		end
-		try
-			PLSRegressor.check_constant_cols([1.0; 1; 1][:,:])
-		catch
-			@test true
-		end
+		@test_throws Exception PLSRegressor.check_constant_cols([1.0 1;1 2;1 3])
+		@test_throws Exception PLSRegressor.check_constant_cols([1.0;1;1][:,:])
+		@test_throws Exception PLSRegressor.check_constant_cols([1.0 2 3])
+		@test_throws Exception PLSRegressor.check_constant_cols([1.0; 1; 1][:,:])
 
 		@test PLSRegressor.check_constant_cols([1.0 1;2 2;3 3])
 		@test PLSRegressor.check_constant_cols([1.0;2;3][:,:])
-
 	end
 
 	@testset "centralize" begin
-
 		X        = [1; 2; 3.0][:,:]
 		X        = PLSRegressor.centralize_data(X,mean(X,dims=1),std(X,dims=1))
-		@test all(X .== [-1;0;1.0])
 
+		@test all(X .== [-1;0;1.0])
 	end
 
 	@testset "decentralize" begin
-
 		Xo        = [1; 2; 3.0][:,:]
 		Xn        = [-1;0;1.0][:,:]
 		Xn        = PLSRegressor.decentralize_data(Xn,mean(Xo,dims=1),std(Xo,dims=1))
-		@test all(Xn .== [1; 2; 3.0])
 
+		@test all(Xn .== [1; 2; 3.0])
 	end
 
 	@testset "checkdata" begin
-
-         try
-			 PLSRegressor.check_params(2,1,"linear")
-		 catch
-			 @test true
-		 end
-		 try
-			 PLSRegressor.check_params(-1,2,"linear")
-		 catch
-			 @test true
-		 end
-		 try
-			 PLSRegressor.check_params(1,2,"x")
-		 catch
-			 @test true
-		 end
+		 @test_throws Exception PLSRegressor.check_params(2,1,"linear")
+		 @test_throws Exception PLSRegressor.check_params(-1,2,"linear")
+		 @test_throws Exception PLSRegressor.check_params(1,2,"x")
 
 		 @test PLSRegressor.check_params(1,2,"linear")
-
 	end
 
 	@testset "checkparams" begin
+		 @test_throws Exception PLSRegressor.check_data(zeros(0,0), 0)
+		 @test_throws Exception PLSRegressor.check_data(zeros(1,1), 10)
 
-		 try
-			 PLSRegressor.check_data(zeros(0,0), 0)
-		 catch
-			 @test true
-		 end
-		 try
-			 PLSRegressor.check_data(zeros(1,1), 10)
-		 catch
-			 @test true
-		 end
 		 @test PLSRegressor.check_data(zeros(1,1), 1)
-
 	end
-
-end;
+end
