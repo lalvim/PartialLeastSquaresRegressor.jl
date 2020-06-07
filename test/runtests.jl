25e04f72850f9738b0826e6f5c7f69a55f66cef1
alvim <alvim.lgm@gmail.com>
2020-06-07 00:12:10 -0300

    clean

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,8 +5,7 @@ using MLJ
 using Statistics
 using Random
 
+include("./utils_test.jl")
 include("./pls1_test.jl")
 include("./pls2_test.jl")
-include("./utils_test.jl")
 include("./kpls_test.jl")
-include("./interface.jl")
